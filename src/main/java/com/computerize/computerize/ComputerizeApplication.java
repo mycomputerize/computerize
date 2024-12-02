067e6444fd1b872e6ae3b2bb0f42277f2bf5351f
ahmad.bader <ahmad.bader@ttgint.com>
2024-12-02 22:16:43 +0300

    first commit

---
+++ src/main/java/com/computerize/computerize/ComputerizeApplication.java
@@ -0,0 +1,13 @@
+package com.computerize.computerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerizeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/computerize/computerize/ComputerizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.computerize.computerize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
